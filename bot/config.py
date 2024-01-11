d9309aafe8ec9153011f1982dbfaea9dfae80555
pshaung <speshiou@gmail.com>
2024-01-11 10:07:16 +0800

    Migrate the major domain

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,10 @@
 import os, csv
 
+
 # dummy gettext function for parsing pot file
 def _(text):
     return text
+
 
 DEFAULT_MODELS = {
     "gpt": {
@@ -13,7 +15,7 @@ DEFAULT_MODELS = {
     },
     "gemini": {
         "name": "Gemini",
-    }
+    },
 }
 
 DEFAULT_CHAT_MODES = {
@@ -22,26 +24,30 @@ DEFAULT_CHAT_MODES = {
         "icon": "🤖",
         "name": "ChatGPT",
         "greeting": _("Hi! This is ChatGPT. How can I assist you today?"),
-        "prompt": "You are a helpful assistant named ChatGPT powered by OpenAI's GPT model."
+        "prompt": "You are a helpful assistant named ChatGPT powered by OpenAI's GPT model.",
     },
     "proofreader": {
         "id": "proofreader",
         "icon": "📝",
         "name": _("Proofreader"),
         "disable_history": True,
-        "greeting": _("Hi, I'm Proofreader. Now you can give me any text in any languages, I will help you check grammar, spelling and wording usage, then rephrase it and do proofreading."),
+        "greeting": _(
+            "Hi, I'm Proofreader. Now you can give me any text in any languages, I will help you check grammar, spelling and wording usage, then rephrase it and do proofreading."
+        ),
         "prompt": """As a Proofreader, your primary goal is to help users to improve their language skill, rephrase their sentences to be more like native speakers without changing the language they are saying. For example, when users speak Japanese to you, then you only response rephrased Japanese. rewrite the sentences.
         All your answers MUST follow the structure below (keep the Markdown tags):
 the rephrased text goes here
 
-and point out all the grammar, spelling and wording mistakes in detail as a list, and describe how you fix the errors, wrap some emphasized words with markdown tags like `{WORD}`, compliment them when they were doing well."""
+and point out all the grammar, spelling and wording mistakes in detail as a list, and describe how you fix the errors, wrap some emphasized words with markdown tags like `{WORD}`, compliment them when they were doing well.""",
     },
     "dictionary": {
         "id": "dictionary",
         "icon": "📔",
         "name": _("Dictionary"),
         "disable_history": True,
-        "greeting": _("This is a dictionary where you can search for any words or phrases in various languages."),
+        "greeting": _(
+            "This is a dictionary where you can search for any words or phrases in various languages."
+        ),
         "prompt": """As a dictionary, all of your responses MUST follow the structure below:
 `the inquired word or phrase` along with its pronunciation in phonetic transcription and an explanation of its part of speech, meaning, and usage
 
@@ -56,9 +62,10 @@ list 5 of example sentences.
     },
 }
 
-CHAT_MODES = { **DEFAULT_CHAT_MODES }
+CHAT_MODES = {**DEFAULT_CHAT_MODES}
 
-def _env_parse_int(name, default_value = None):
+
+def _env_parse_int(name, default_value=None):
     value = os.getenv(name, default_value)
     if value:
         if isinstance(value, int):
@@ -69,22 +76,25 @@ def _env_parse_int(name, default_value = None):
             raise Exception(f"{name} isn't an integer")
     return value if value else default_value
 
-def _env_parse_float(name, default_value = None):
+
+def _env_parse_float(name, default_value=None):
     value = os.getenv(name, default_value)
     if value:
         if isinstance(value, float):
             return value
-        if value.replace('.', '', 1).isdigit():
+        if value.replace(".", "", 1).isdigit():
             return float(value)
         else:
             raise Exception(f"{name} isn't an number")
     return value if value else default_value
 
-def _env_parse_str_array(name, default_value = None):
+
+def _env_parse_str_array(name, default_value=None):
     value = os.getenv(name, default_value)
     if value:
         return value.split(",")
     return value
+
 
 def load_prompts(tsv):
     if not os.path.isfile(tsv):
@@ -104,6 +114,7 @@ def load_prompts(tsv):
             }
     return prompts
 
+
 def load_tts_models(tsv):
     models = {}
     with open(tsv) as file:
@@ -114,53 +125,58 @@ def load_tts_models(tsv):
             models[key] = model
     return models
 
-MONGODB_HOST = os.getenv('MONGODB_HOST')
-MONGODB_PORT = os.getenv('MONGODB_PORT')
-MONGODB_DATABASE = os.getenv('MONGODB_DATABASE')
+
+MONGODB_HOST = os.getenv("MONGODB_HOST")
+MONGODB_PORT = os.getenv("MONGODB_PORT")
+MONGODB_DATABASE = os.getenv("MONGODB_DATABASE")
 MONGODB_URI = f"mongodb://{MONGODB_HOST}:{MONGODB_PORT}"
 
-FREE_QUOTA = _env_parse_int('FREE_QUOTA', 10000)
+FREE_QUOTA = _env_parse_int("FREE_QUOTA", 10000)
 # default price for gpt-3.5-turbo
-TOKEN_PRICE = _env_parse_float('TOKEN_PRICE', 0.002)
+TOKEN_PRICE = _env_parse_float("TOKEN_PRICE", 0.002)
 # DALL·E tokens
-DALLE_TOKENS = _env_parse_int('DALLE_TOKENS', 10000)
+DALLE_TOKENS = _env_parse_int("DALLE_TOKENS", 10000)
 # cost for real-esrgan-4x upscaler
-UPSCALE_COST = _env_parse_int('UPSCALE_COST', 9000)
-IMAGE_TIMEOUT = _env_parse_int('IMAGE_TIMEOUT', 60)
+UPSCALE_COST = _env_parse_int("UPSCALE_COST", 9000)
+IMAGE_TIMEOUT = _env_parse_int("IMAGE_TIMEOUT", 60)
 # prompts
-if os.getenv('GPT_PROMPTS'):
-    CHAT_MODES = { **CHAT_MODES, **load_prompts(os.getenv('GPT_PROMPTS')) }
+if os.getenv("GPT_PROMPTS"):
+    CHAT_MODES = {**CHAT_MODES, **load_prompts(os.getenv("GPT_PROMPTS"))}
 
 # coqui
-COQUI_STUDIO_TOKEN = os.getenv('COQUI_STUDIO_TOKEN')
+COQUI_STUDIO_TOKEN = os.getenv("COQUI_STUDIO_TOKEN")
 TTS_MODELS = {}
 # TTS models
-if os.getenv('TTS_MODELS'):
-    TTS_MODELS = { **TTS_MODELS, **load_tts_models(os.getenv('TTS_MODELS')) }
-AUDIO_FILE_TMP_DIR = 'tmp/voice'
+if os.getenv("TTS_MODELS"):
+    TTS_MODELS = {**TTS_MODELS, **load_tts_models(os.getenv("TTS_MODELS"))}
+AUDIO_FILE_TMP_DIR = "tmp/voice"
 
 SUPPORT_USER_NAME = "nexia_support"
-TELEGRAM_BOT_NAME = os.getenv('TELEGRAM_BOT_NAME')
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELEGRAM_BOT_NAME = os.getenv("TELEGRAM_BOT_NAME")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 MESSAGE_MAX_LENGTH = 4000
 # OpenAI official API
 DEFAULT_OPENAI_API_TYPE = "open_ai"
-CGP_PROJECT_ID = os.getenv('CGP_PROJECT_ID')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-OPENAI_CHAT_API_TYPE = os.getenv('OPENAI_CHAT_API_TYPE', DEFAULT_OPENAI_API_TYPE)
+CGP_PROJECT_ID = os.getenv("CGP_PROJECT_ID")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_CHAT_API_TYPE = os.getenv("OPENAI_CHAT_API_TYPE", DEFAULT_OPENAI_API_TYPE)
 if not OPENAI_CHAT_API_TYPE:
     OPENAI_CHAT_API_TYPE = DEFAULT_OPENAI_API_TYPE
 # OpenAI API on Azure
-AZURE_OPENAI_API_BASE = os.getenv('AZURE_OPENAI_API_BASE')
-AZURE_OPENAI_API_VERSION = os.getenv('AZURE_OPENAI_API_VERSION')
-AZURE_OPENAI_API_KEY = os.getenv('AZURE_OPENAI_API_KEY')
-if not AZURE_OPENAI_API_BASE or not AZURE_OPENAI_API_VERSION or not AZURE_OPENAI_API_KEY:
+AZURE_OPENAI_API_BASE = os.getenv("AZURE_OPENAI_API_BASE")
+AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
+AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+if (
+    not AZURE_OPENAI_API_BASE
+    or not AZURE_OPENAI_API_VERSION
+    or not AZURE_OPENAI_API_KEY
+):
     # fallback to official OpenAI base if Azure is not set up properly
     OPENAI_CHAT_API_TYPE = DEFAULT_OPENAI_API_TYPE
 # getimg.ai
-GETIMG_API_TOKEN = os.getenv('GETIMG_API_TOKEN')
+GETIMG_API_TOKEN = os.getenv("GETIMG_API_TOKEN")
 # sinkin.ai
-SINKIN_ACCOUNT = os.getenv('SINKIN_ACCOUNT')
+SINKIN_ACCOUNT = os.getenv("SINKIN_ACCOUNT")
 # request timeout in seconds
 OPENAI_TIMEOUT = 60
 # whisper api has 25MB of file size limit, set 20MB to maintain buffer
@@ -174,17 +190,21 @@ COQUI_TOKENS = 200
 # duration per character in second
 TTS_ESTIMATED_DURATION_BASE = 0.05
 STREAM_ENABLED = True
-ALLOWED_TELEGRAM_USERNAMES = _env_parse_str_array('ALLOWED_TELEGRAM_USERNAMES')
+ALLOWED_TELEGRAM_USERNAMES = _env_parse_str_array("ALLOWED_TELEGRAM_USERNAMES")
 DEFAULT_CHAT_MODE = list(DEFAULT_CHAT_MODES.keys())[0]
-DEFAULT_MODEL = os.getenv('DEFAULT_MODEL')
+DEFAULT_MODEL = os.getenv("DEFAULT_MODEL")
 if not DEFAULT_MODEL:
     DEFAULT_MODEL = list(DEFAULT_MODELS.keys())[0]
-ENABLE_SELECT_MODEL = os.getenv('ENABLE_SELECT_MODEL', True)
-ENABLE_SELECT_MODEL = True if not ENABLE_SELECT_MODEL or ENABLE_SELECT_MODEL == True else False
-ENABLE_CUSTOM_ROLE = os.getenv('ENABLE_CUSTOM_ROLE', True)
-ENABLE_CUSTOM_ROLE = True if not ENABLE_CUSTOM_ROLE or ENABLE_CUSTOM_ROLE == True else False
-DEFAULT_CHAT_TIMEOUT = _env_parse_int('DEFAULT_CHAT_TIMEOUT', 60 * 60 * 1)
-API_ENDPOINT = os.getenv('API_ENDPOINT')
-WEB_APP_URL = os.getenv('WEB_APP_URL')
-BUGREPORT_BOT_TOKEN = os.getenv('BUGREPORT_BOT_TOKEN')
-BUGREPORT_CHAT_ID = os.getenv('BUGREPORT_CHAT_ID')
+ENABLE_SELECT_MODEL = os.getenv("ENABLE_SELECT_MODEL", True)
+ENABLE_SELECT_MODEL = (
+    True if not ENABLE_SELECT_MODEL or ENABLE_SELECT_MODEL == True else False
+)
+ENABLE_CUSTOM_ROLE = os.getenv("ENABLE_CUSTOM_ROLE", True)
+ENABLE_CUSTOM_ROLE = (
+    True if not ENABLE_CUSTOM_ROLE or ENABLE_CUSTOM_ROLE == True else False
+)
+DEFAULT_CHAT_TIMEOUT = _env_parse_int("DEFAULT_CHAT_TIMEOUT", 60 * 60 * 1)
+API_ENDPOINT = os.getenv("API_ENDPOINT")
+WEB_APP_URL = os.getenv("WEB_APP_URL")
+BUGREPORT_BOT_TOKEN = os.getenv("BUGREPORT_BOT_TOKEN")
+BUGREPORT_CHAT_ID = os.getenv("BUGREPORT_CHAT_ID")
--- bot/ui.py
+++ bot/ui.py
@@ -8,6 +8,7 @@ import i18n
 from database import Database
 import helper
 
+
 def get_arg(path: str, key: str):
     if "?" not in path:
         return None
@@ -15,13 +16,14 @@ def get_arg(path: str, key: str):
     query_params = parse_qs(query)
     return query_params[key][0] if key in query_params else None
 
+
 def add_arg(path: str, key: str, value: str):
     base_path = path
     if "?" in path:
         base_path, query = path.split("?")
         query_params = parse_qs(query)
         # convert query params to a flat dictionary with string values
-        query_params = {k:v[0] for k,v in query_params.items()}
+        query_params = {k: v[0] for k, v in query_params.items()}
     else:
         base_path = path
         query_params = {}
@@ -29,15 +31,17 @@ def add_arg(path: str, key: str, value: str):
     query_string = urlencode(query_params)
     return base_path + "?" + query_string
 
+
 def get_args(path: str):
     if "?" in path:
         base_path, query = path.split("?")
         query_params = parse_qs(query)
         # convert query params to a flat dictionary with string values
-        query_params = {k:v[0] for k,v in query_params.items()}
+        query_params = {k: v[0] for k, v in query_params.items()}
     else:
         query_params = {}
     return query_params
+
 
 def add_args(path: str, args: dict):
     base_path = path
@@ -45,7 +49,7 @@ def add_args(path: str, args: dict):
         base_path, query = path.split("?")
         query_params = parse_qs(query)
         # convert query params to a flat dictionary with string values
-        query_params = {k:v[0] for k,v in query_params.items()}
+        query_params = {k: v[0] for k, v in query_params.items()}
     else:
         base_path = path
         query_params = {}
@@ -55,6 +59,7 @@ def add_args(path: str, args: dict):
     }
     query_string = urlencode(query_params)
     return base_path + "?" + query_string
+
 
 def _chat_mode_features(chat_mode=None):
     if chat_mode is None:
@@ -69,6 +74,7 @@ def _chat_mode_features(chat_mode=None):
         bonus.append("⚡")
     return bonus
 
+
 def _chat_mode_options(_):
     options = []
     for chat_mode, role in config.CHAT_MODES.items():
@@ -77,24 +83,24 @@ def _chat_mode_options(_):
 
         if len(features) > 0:
             label += " ({})".format("".join(features))
-        options.append({ 
-            "label": label, 
-            "value": chat_mode, 
-            "callback": "set_chat_mode|" + chat_mode 
-        })
+        options.append(
+            {
+                "label": label,
+                "value": chat_mode,
+                "callback": "set_chat_mode|" + chat_mode,
+            }
+        )
     return options
+
 
 def _model_options(_):
     options = []
     for key, model in config.DEFAULT_MODELS.items():
         label = model["name"]
 
-        options.append({ 
-            "label": label, 
-            "value": key, 
-            "callback": "set_model|" + key 
-        })
+        options.append({"label": label, "value": key, "callback": "set_model|" + key})
     return options
+
 
 def chat_mode_tips(chat_mode, _):
     features = _chat_mode_features(chat_mode)
@@ -120,11 +126,16 @@ def chat_mode_tips(chat_mode, _):
     text = build_tips(tips, _, hide_bullet=True, title=_("<b>Features</b>"))
     if chat_mode is not None and "⚡" in features:
         text += "\n\n"
-        text += build_tips([
-            _("desktop: type /{}, then press TAB key").format(chat_mode[0]),
-            _("mobile: type /{}, then long press the command").format(chat_mode[0]),
-        ], _, title=_("<b>How to do instant access?</b>"))
+        text += build_tips(
+            [
+                _("desktop: type /{}, then press TAB key").format(chat_mode[0]),
+                _("mobile: type /{}, then long press the command").format(chat_mode[0]),
+            ],
+            _,
+            title=_("<b>How to do instant access?</b>"),
+        )
     return text
+
 
 def load_settings(db: Database, chat_id: int, _):
     current_chat_mode = helper.get_current_chat_mode(db, chat_id)
@@ -139,10 +150,16 @@ def load_settings(db: Database, chat_id: int, _):
         "current_model": {
             "icon": "💬",
             "name": _("AI Model"),
-            "desc": chat_mode_tips(None, _) + "\n\n" + build_tips([
-                _("🤥 Some characters are made up! Don't take them too seriously."),
-                _("🤩 More roles are coming soon. Stay tuned!"),
-            ], _, hide_bullet=True),
+            "desc": chat_mode_tips(None, _)
+            + "\n\n"
+            + build_tips(
+                [
+                    _("🤥 Some characters are made up! Don't take them too seriously."),
+                    _("🤩 More roles are coming soon. Stay tuned!"),
+                ],
+                _,
+                hide_bullet=True,
+            ),
             "hide_tips_bullet": True,
             "value": current_model,
             "disable_check_mark": True,
@@ -152,15 +169,21 @@ def load_settings(db: Database, chat_id: int, _):
         "current_chat_mode": {
             "icon": "💬",
             "name": _("Chat Mode"),
-            "desc": chat_mode_tips(None, _) + "\n\n" + build_tips([
-                _("🤥 Some characters are made up! Don't take them too seriously."),
-                _("🤩 More roles are coming soon. Stay tuned!"),
-            ], _, hide_bullet=True),
+            "desc": chat_mode_tips(None, _)
+            + "\n\n"
+            + build_tips(
+                [
+                    _("🤥 Some characters are made up! Don't take them too seriously."),
+                    _("🤩 More roles are coming soon. Stay tuned!"),
+                ],
+                _,
+                hide_bullet=True,
+            ),
             "hide_tips_bullet": True,
             "value": current_chat_mode,
             "disable_check_mark": True,
             "num_keyboard_cols": 2,
-            "options": _chat_mode_options(_)
+            "options": _chat_mode_options(_),
         },
         # "voice_mode": {
         #     "icon": "🗣",
@@ -221,7 +244,9 @@ def load_settings(db: Database, chat_id: int, _):
         "lang": {
             "icon": "🌐",
             "name": _("Language"),
-            "desc": _("This setting won't effect the answers from the chatbot.\n\nPlease feedback to @{} if there is any translation errors.").format(config.SUPPORT_USER_NAME),
+            "desc": _(
+                "This setting won't effect the answers from the chatbot.\n\nPlease feedback to @{} if there is any translation errors."
+            ).format(config.SUPPORT_USER_NAME),
             "value": lang,
             "options": [
                 {
@@ -248,11 +273,12 @@ def load_settings(db: Database, chat_id: int, _):
                     "label": _("Not specify"),
                     "value": None,
                 },
-            ]
+            ],
         },
     }
 
     return settings
+
 
 def build_tips(tips, _, title=None, hide_bullet=False):
     bullet = "- " if not hide_bullet else ""
@@ -265,6 +291,7 @@ def build_tips(tips, _, title=None, hide_bullet=False):
     text += "\n".join(map(lambda tip: bullet + _(tip), tips))
     return text
 
+
 def build_keyboard_rows(buttons, num_keyboard_cols):
     keyboard_rows = []
     num_buttons = len(buttons)
@@ -272,6 +299,7 @@ def build_keyboard_rows(buttons, num_keyboard_cols):
         end = min(i + num_keyboard_cols, num_buttons)
         keyboard_rows.append(buttons[i:end])
     return keyboard_rows
+
 
 def _menu_page(path: str, menu_data, _):
     path = path if path is not None else menu_data["key"]
@@ -333,19 +361,25 @@ def _menu_page(path: str, menu_data, _):
                 label = option["name"]
                 if "icon" in option:
                     label = "{} {}".format(option["icon"], label)
-                
+
                 keyboard.append(InlineKeyboardButton(label, **args))
         else:
             # array
             for option in data["options"]:
                 label = option["label"]
-                if "disable_check_mark" not in data and "value" in data and data["value"] == option["value"]:
+                if (
+                    "disable_check_mark" not in data
+                    and "value" in data
+                    and data["value"] == option["value"]
+                ):
                     label = "✅ " + label
 
                 if "callback" in option:
                     callback_data = option["callback"]
                 else:
-                    callback_value = option["value"] if option["value"] is not None else ""
+                    callback_value = (
+                        option["value"] if option["value"] is not None else ""
+                    )
                     # back to previous menu after selecting an option
                     base_path = ">".join(path_segs[:-1])
                     callback_data = f"{base_path}|{menu_page_key}|{callback_value}"
@@ -353,21 +387,28 @@ def _menu_page(path: str, menu_data, _):
                         callback_data += "?" + query
                 if "args" in data:
                     callback_data = add_args(callback_data, data["args"])
-                keyboard.append(InlineKeyboardButton(label, callback_data=callback_data))
+                keyboard.append(
+                    InlineKeyboardButton(label, callback_data=callback_data)
+                )
 
     keyboard_rows = []
     if len(keyboard) > 0:
-        keyboard_rows = build_keyboard_rows(keyboard, num_keyboard_cols=num_keyboard_cols)
+        keyboard_rows = build_keyboard_rows(
+            keyboard, num_keyboard_cols=num_keyboard_cols
+        )
     if len(segs) > 1:
         callback_data = ">".join(path_segs[:-1])
         if query:
             callback_data += "?" + query
-        keyboard_rows.append([InlineKeyboardButton("< " + _("Back"), callback_data=callback_data)])
+        keyboard_rows.append(
+            [InlineKeyboardButton("< " + _("Back"), callback_data=callback_data)]
+        )
     else:
         keyboard_rows.append([InlineKeyboardButton(_("Close"), callback_data="close")])
     if len(keyboard_rows) > 0:
         reply_markup = InlineKeyboardMarkup(keyboard_rows)
     return text, reply_markup
+
 
 def settings(db: Database, chat_id: int, _, data: str = None):
     if data and "|" in data:
@@ -381,7 +422,7 @@ def settings(db: Database, chat_id: int, _, data: str = None):
         settings = load_settings(db, chat_id, _)
         if setting_key in settings:
             db.set_chat_attribute(chat_id, setting_key, value)
-        if setting_key == 'lang':
+        if setting_key == "lang":
             _ = i18n.get_text_func(value)
     else:
         path = data
@@ -427,35 +468,53 @@ def settings(db: Database, chat_id: int, _, data: str = None):
 
     return _menu_page(path, menu_data, _)
 
+
 def about(_):
-    text = _("Hi! My name is Nexia, an AI chatbot powered by OpenAI's GPT, DALL·E and Stable Diffusion models.")
+    text = _(
+        "Hi! My name is Nexia, an AI chatbot powered by OpenAI's GPT, DALL·E and Stable Diffusion models."
+    )
     text += "\n\n"
     text += _("<b>Additional features</b>\n")
     text += _("🧙‍♀️ Customizable chatbot\n")
     text += _("🎙 Support voice messages\n")
     text += _("🌐 Summarize the content in links") + "\n"
-    text += _("🎬 Summarize Youtube videos (up to 20 minutes long)") + "\n" 
-    text += _("👥 Group chat - add this bot to a group chat, then type /chatgpt to start.\n").format(config.TELEGRAM_BOT_NAME)
+    text += _("🎬 Summarize Youtube videos (up to 20 minutes long)") + "\n"
+    text += (
+        _(
+            """👥 Group chat - add this bot to a group chat, then type /chatgpt to start."""
+        ).format(config.TELEGRAM_BOT_NAME)
+        + "\n"
+    )
     text += "✍️ /proofreader - " + _("rewrite sentences\n")
     text += "📔 /dictionary - " + _("learn about words/phrases\n")
     text += "👨‍🎨 /image - " + _("generate images from text\n")
     text += "\n\n"
-    text += _("""By using this chatbot, you agree to our <a href="{}">terms of service</a> and <a href="{}">privacy policy</a>.""").format("https://tgchat.co/terms-of-service", "https://tgchat.co/privacy-policy")
+    text += _(
+        """By using this chatbot, you agree to our <a href="{}">terms of service</a> and <a href="{}">privacy policy</a>."""
+    ).format("https://nexia.chat/terms-of-service", "https://nexia.chat/privacy-policy")
 
-    reply_markup = InlineKeyboardMarkup([
+    reply_markup = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton("⚙️ " + _("Settings"), web_app=WebAppInfo(url=config.WEB_APP_URL)),
-            InlineKeyboardButton("💡 " + _("Learn"), url="https://t.me/nexia_news"),
-        ],
-        [
-            InlineKeyboardButton("❓ " + _("FAQ"), url="https://tgchat.co/faq"),
-            InlineKeyboardButton("✉️ " + _("Feedback"), url="https://t.me/{}".format(config.SUPPORT_USER_NAME)),
+            [
+                InlineKeyboardButton(
+                    "⚙️ " + _("Settings"), web_app=WebAppInfo(url=config.WEB_APP_URL)
+                ),
+                InlineKeyboardButton("💡 " + _("Learn"), url="https://t.me/nexia_news"),
+            ],
+            [
+                InlineKeyboardButton("❓ " + _("FAQ"), url="https://nexia.chat/faq"),
+                InlineKeyboardButton(
+                    "✉️ " + _("Feedback"),
+                    url="https://t.me/{}".format(config.SUPPORT_USER_NAME),
+                ),
+            ],
         ]
-    ])
+    )
 
     return text, reply_markup
 
-def image_menu(_, path = None):
+
+def image_menu(_, path=None):
     options = {}
 
     for key, value in gen_image_utils.MODELS.items():
@@ -466,21 +525,21 @@ def image_menu(_, path = None):
             height = size["height"]
             cost = size["cost"]
             label = "{}x{} (💰 {:,.0f})".format(width, height, cost)
-            callback_data = add_args("gen_image", {
-                **get_args(path),
-                "w": width,
-                "h": height,
-            })
-            size_options.append({
-                "label": label,
-                "callback": callback_data
-            })
+            callback_data = add_args(
+                "gen_image",
+                {
+                    **get_args(path),
+                    "w": width,
+                    "h": height,
+                },
+            )
+            size_options.append({"label": label, "callback": callback_data})
 
         desc = _("Select the image size (width x height)")
         if "tips" in value:
             desc += "\n\n" + build_tips(value["tips"], _)
 
-        options[key] = { 
+        options[key] = {
             "icon": "🎨",
             "name": _(value["name"]),
             "desc": desc,
